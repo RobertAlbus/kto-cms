61c9b52b1fe28bcab05b655e2e51d618426231e9
Robert Albus <robert.albus@kontralto.com>
2021-06-03 14:33:27 -0600

    Set up Piranha CMS to use Docker build and compose for application and PostgreSQL database.

---
+++ piranha-kto/Kernel/Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace piranha_kto.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
---
+++ piranha-kto/Kernel/README.md
@@ -0,0 +1,6 @@
+https://piranhacms.org/docs/v7/basics/getting-started
+https://piranhacms.org/docs/v7/basics/project-templates
+
+cd your/project/dir
+dotnet build .
+dotnet run
