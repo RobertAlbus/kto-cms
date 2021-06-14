b20acdd2452592a45ab0b289ecdcb29b5eede554
Robert Albus <robert.albus@kontralto.com>
2021-06-14 14:17:26 -0600

    Add Cofoundry CMS.

---
+++ cofoundry-kto/Kernel/Cofoundry/PageBlockTypes/SuperBanner/ContentSectionDataModel.cs
@@ -0,0 +1,29 @@
+﻿using Cofoundry.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Kontralto_Site
+{
+    /// <summary>
+    /// An example page block type. 
+    /// See https://github.com/cofoundry-cms/cofoundry/wiki/Page-Block-Types
+    /// for more information
+    /// </summary>
+    public class ContentSectionDataModel : IPageBlockTypeDataModel, IPageBlockTypeDisplayModel
+    {
+        [Required]
+        [Display]
+        public string Heading { get; set; }
+
+        [Required]
+        [Display]
+        public string Subheading { get; set; }
+
+        [Required]
+        [Display(Name = "Body", Description = "Rich text displayed at full width")]
+        [Html(HtmlToolbarPreset.BasicFormatting, HtmlToolbarPreset.Headings, HtmlToolbarPreset.Media)]
+        public string Body { get; set; }
+    }
+}
