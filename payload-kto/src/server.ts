e6cb7df3b9860488ac8d098fa6978e4bb70e941e
Robert Albus <robert.albus@kontralto.com>
2021-06-23 15:32:13 -0600

    For debugging [drop this commit later].

--- payload-kto/src/server.ts
+++ payload-kto/src/server.ts
@@ -1,5 +1,23 @@
 import express from 'express';
-import payload from 'payload';
+import payload, { Payload } from 'payload';
+
+function expandedLog(item, maxDepth = 100, depth = 0){
+  if (depth > maxDepth ) {
+      console.log(item);
+      return;
+  }
+  if (typeof item === 'object' && item !== null) {
+      Object.entries(item).forEach(([key, value]) => {
+          console.group(key + ' : ' +(typeof value));
+          expandedLog(value, maxDepth, depth + 1);
+          console.groupEnd();
+      });
+  } else {
+      console.log(item);
+  }
+}
+
+
 
 require('dotenv').config();
 const app = express();
@@ -11,6 +29,15 @@ payload.init({
   express: app,
   onInit: () => {
     payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+    // expandedLog(payload.config)
+
+    // console.log("=============================================================")
+    // console.log("=============================================================")
+    // console.log("=============================================================")
+    // console.log("=============================================================")
+    // console.log("=============================================================")
+    // console.log("=============================================================")
+    // console.log(payload.config)
   },
 });
 
